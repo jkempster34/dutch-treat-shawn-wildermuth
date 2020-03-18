ac44a8463005ef2b2b1fde6e9f23a895475e8747
Joseph Kempster <jkempster34@gmail.com>
2020-03-18 18:15:58 +0000

    Add simple HTML form
    - Demonstrate how to serve static files in ASP.NET.
    - Demonstrate manipulating the DOM using vanilla JavaScript.
    - Demonstrate styling with CSS.

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,10 @@
+﻿
+
+const theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+const button = document.getElementById("buyButton");
+button.addEventListener("click", () => console.log("Buying item"));
+
+const productInfo = document.getElementsByClassName("product-props");
+const listItems = productInfo.item[0].children;
