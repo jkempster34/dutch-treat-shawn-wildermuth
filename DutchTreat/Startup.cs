ea2f3e106f3c87d8c97e9288cf794f27a71d0d66
Joseph Kempster <jkempster34@gmail.com>
2020-03-20 17:34:16 +0000

    Add contact and about us views

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -35,20 +35,19 @@ namespace DutchTreat
             app.UseNodeModules();
 
             /* 
-             * When a URL that hits the app, it is interpreted and mapped to the correct controller.
+             * When a URL hits the app, it is interpreted and mapped to the correct controller.
              * In previous versions, every feature (MVC, Razor Pages, SIgnalR, etc.), had it’s own endpoint implementation. Now 
              * the endpoint and routing configuration can be done independently.
              * 
-             * UseRouting turns on generic endpoint routing in ASP.net Core 3+. It defines where, in the middleware pipeline,
+             * UseRouting() turns on generic endpoint routing in ASP.net Core 3+. It defines where, in the middleware pipeline,
              * the routing decisions are made.
              * 
-             * UseEndpoints creates a data structure containing all patterns when the application starts. In other words,
-             * it declares the mapping of effective routes
+             * UseEndpoints() creates a data structure containing all patterns when the application starts. In other words,
+             * it declares the mapping of effective routes.
              * 
              * MapControllerRoute defines default mapping. The first parameter is a unique name given to a route, the second specifies
-             * a URL pattern, the third are defaults
+             * a URL pattern, the third are defaults.
              * 
-             
              */
 
             app.UseRouting();
@@ -56,8 +55,10 @@ namespace DutchTreat
             {
                 endpoints.MapControllerRoute(
                     name: "Fallback",
-                    pattern: "{controller}/{action}/{id?}", // URL will be <root>/ UNFINISHED COMMENT
-                    defaults: new { controller = "App", action = "Index" }
+                    pattern: "{controller}/{action}/{id?}", // URL with parameters
+                    // controller refers to contents of controllers folder (.cs file name with "contoller" suffix removed), action refers to the contents of that controller.
+                    // For example, going to <root>/App/Index will look in the controllers folder for "AppController", and within AppController, look for the "Index" action.
+                    defaults: new { controller = "App", action = "Index" } // Parameter defaults. Default URL will be <root>/app/index
                     );
             });
         }
