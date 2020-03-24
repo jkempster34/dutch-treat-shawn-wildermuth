1f0139d8a9f93b1355af5fe6a245d4663e7deaf5
Joseph Kempster <jkempster34@gmail.com>
2020-03-24 14:07:47 +0000

    Add contact page

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -16,9 +16,12 @@ namespace DutchTreat.Controllers
         [HttpGet("contact")]
         public IActionResult Contact()
         {
-            ViewBag.Title = "Contact Us";
+            return View();
+        }
 
-            throw new InvalidOperationException("Bad request");
+        [HttpPost("contact")]
+        public IActionResult Contact(object model)
+        {
             return View();
         }
 
