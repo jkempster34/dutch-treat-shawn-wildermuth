6089c0db97f5398b308f2d9396e3bcbe2dac7681
Joseph Kempster <jkempster34@gmail.com>
2020-04-27 16:45:11 +0100

    Add products api controller

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -8,6 +8,7 @@ using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -43,6 +44,8 @@ namespace DutchTreat
             // Support for real mail service would go here 
 
             services.AddScoped<IDutchRepository, DutchRepository>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
